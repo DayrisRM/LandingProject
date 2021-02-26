a0c32e3d0cbc09470accf92dff2a0246a1ab0050
DayRM <dayris0817@gmail.com>
2021-02-26 12:47:41 +0100

    Adds ILandingResolver, LandingResolver implementation and LandingResolverUnitTests.

---
+++ LandingProject/Abstractions/ILandingResolver.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LandingProject.Abstractions
+{
+    public interface ILandingResolver<TInput, TOutput>
+    {
+        TOutput CheckTrajectory(TInput coordenate);
+    }
+}
+
---
+++ LandingProject/LandingResolver.cs
@@ -0,0 +1,17 @@
+﻿using LandingProject.Abstractions;
+using LandingProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LandingProject
+{
+    public class LandingResolver
+        : ILandingResolver<Coordinate, string>
+    {
+        public string CheckTrajectory(Coordinate coordenate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
